cec114d89902bc69d32c35b8c53441b368587752
peterAnami <peter.andake@student.moringaschool.com>
2021-09-19 18:10:10 +0300

    test

--- app/models.py
+++ app/models.py
@@ -23,16 +23,16 @@ class User(db.Model):
 #   upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
 #   downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
 
-#   @property
-#   def set_password(self):
-#     raise AttributeError('You cannot read the password attribute')
+  @property
+  def set_password(self):
+    raise AttributeError('You cannot read the password attribute')
 
-#   @set_password.setter
-#   def password(self, password):
-#     self.secure_password = generate_password_hash(password)
+  @set_password.setter
+  def password(self, password):
+    self.secure_password = generate_password_hash(password)
 
-#   def verify_password(self, password):
-#     return check_password_hash(self.secure_password,password) 
+  def verify_password(self, password):
+    return check_password_hash(self.secure_password,password) 
   
 #   def save_u(self):
 #     db.session.add(self)

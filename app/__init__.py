3c9a79a1d34d5f33eed483e19a7b67e6d762aa93
peterAnami <peter.andake@student.moringaschool.com>
2021-09-19 18:20:37 +0300

    app auth views

--- app/__init__.py
+++ app/__init__.py
@@ -17,9 +17,9 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
   app = Flask(__name__)
-  # app.config.from_object(config_options[config_name])
-  # from .auth import auth as authentication_blueprint
-  # from .main import main as main_blueprint
+  app.config.from_object(config_options[config_name])
+  from .auth import auth as authentication_blueprint
+  from .main import main as main_blueprint
 
  
   # app.register_blueprint(authentication_blueprint)

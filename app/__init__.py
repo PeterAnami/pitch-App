2c58283e9f757210be313f38fc1034f0c74ff3d5
peterAnami <peter.andake@student.moringaschool.com>
2021-09-19 18:23:49 +0300

    pass

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # from flask import Flask
 # from config import config_options
 # from flask_mail import Mail
-# from flask_login import LoginManager, login_manager
+from flask_login import LoginManager, login_manager
 # from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 # from flask_uploads import IMAGES, UploadSet,configure_uploads
@@ -9,11 +9,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 # mail = Mail()
 bootstrap = Bootstrap()
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 # photos = UploadSet('photos',IMAGES)
-
 
 def create_app(config_name):
   app = Flask(__name__)

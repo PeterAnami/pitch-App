a1d043ed573fd90783616e7188af0c70a9cb0c18
peterAnami <peter.andake@student.moringaschool.com>
2021-09-19 18:27:49 +0300

    views

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 from . import db
 # ,login_manager
 # from datetime import datetime
-# from flask_login import UserMixin,current_user
+from flask_login import UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
 
 # @login_manager.user_loader

8310942f05e62e9ea5df3b6917594f7935203eaa
peterAnami <peter.andake@student.moringaschool.com>
2021-09-19 18:34:31 +0300

    forms

--- app/models.py
+++ app/models.py
@@ -1,16 +1,15 @@
-# from flask_sqlalchemy.model import Model
-from . import db
-# ,login_manager
+from flask_sqlalchemy.model import Model
+from . import db,login_manager
 # from datetime import datetime
 from flask_login import UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#   return User.query.get(user_id)
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
-  #should be included in the above class 
+   
   __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key = True)
   username = db.Column(db.String(255),unique = True,nullable = False)

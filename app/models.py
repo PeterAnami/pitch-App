adf750cad643b19edfa02e90b88287ef30f83383
peterAnami <peter.andake@student.moringaschool.com>
2021-09-19 18:01:24 +0300

    model update

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from . import db
 # ,login_manager
 # from datetime import datetime
 # from flask_login import UserMixin,current_user
-# from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
 
 # @login_manager.user_loader
 # def load_user(user_id):
@@ -15,7 +15,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   username = db.Column(db.String(255),unique = True,nullable = False)
   email  = db.Column(db.String(255),unique = True,nullable = False)
-#   secure_password = db.Column(db.String(255),nullable = False)
+  secure_password = db.Column(db.String(255),nullable = False)
 #   bio = db.Column(db.String(255))
 #   profile_pic_path = db.Column(db.String())
 #   pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
